1ad0a2af8e1b5cd8c06c4ab00b861354c3e24bd9
jaredlander <info@jaredlander.com>
2014-09-05 17:16:06 -0400

    Cleaned up one line that was used for debugging.

--- R/ShinyBuilder.R
+++ R/ShinyBuilder.R
@@ -8,7 +8,6 @@
 #' @export
 runShinyBuilder <- function(){
   sb_path <- system.file(package = 'ShinyBuilder')
-  #sb_path <- sprintf("%s/", sb_path)
   
   #Check/set permissions
   dir_mode <- as.numeric(as.character(file.info(paste0(sb_path, '/dashboards'))$mode))
